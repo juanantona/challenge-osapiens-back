694c47f8ce8e0801cbb2aae92eecff974e88c28c
Juanan <juan.antonadelval@gmail.com>
2024-12-29 19:18:51 +0100

    test(ChallengeStep1): Adds more test case for PolygonAreaJob function

--- src/jobs/PolygonAreaJob.spec.ts
+++ src/jobs/PolygonAreaJob.spec.ts
@@ -12,11 +12,59 @@ const geoJson = {
       [-46.159527846351466, -16.048997734633218],
     ],
   ],
-  area: 4631853809.79,
+  areaInSquareMeters: 4621506484.056065,
 };
 
 describe('PolygonAreaJob', () => {
-  describe('When running a task with a valid geoJson', () => {
+  let logSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    logSpy = jest.spyOn(global.console, 'log');
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks;
+  });
+
+  describe('When the job runs', () => {
+    it('Should log the task Id at job starting', async () => {
+      const task = new Task();
+      task.taskId = 'task-id';
+      task.geoJson = JSON.stringify(geoJson);
+
+      const job = new PolygonAreaJob();
+      await job.run(task);
+
+      expect(logSpy).toHaveBeenCalledWith(`Running calculating area for task ${task.taskId}...`);
+    });
+  });
+
+  describe('When the job runs a task with an non valid polygon', () => {
+    it('Should throw an error', async () => {
+      const task = new Task();
+      task.taskId = 'task-id';
+      task.geoJson = '{}';
+
+      const job = new PolygonAreaJob();
+
+      await expect(job.run(task)).rejects.toThrow();
+    });
+  });
+
+  describe('When the job runs a task with a valid geoJson', () => {
+    it('Should log area calculation message with taskId', async () => {
+      const task = new Task();
+      task.taskId = 'task-id';
+      task.geoJson = JSON.stringify(geoJson);
+
+      const job = new PolygonAreaJob();
+      await job.run(task);
+
+      expect(logSpy).toHaveBeenCalledWith(
+        `The polygon area is ${geoJson.areaInSquareMeters} square meters.`
+      );
+    });
+
     it('Should return the correct area', async () => {
       const task = new Task();
       task.geoJson = JSON.stringify(geoJson);
@@ -24,7 +72,7 @@ describe('PolygonAreaJob', () => {
       const job = new PolygonAreaJob();
       const result = await job.run(task);
 
-      expect(result).toBe(geoJson.area);
+      expect(result).toBe(geoJson.areaInSquareMeters);
     });
   });
 });
