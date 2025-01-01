46aa8bd3ababbb228567673faf5390c568f327ff
Juanan <juan.antonadelval@gmail.com>
2025-01-01 22:24:16 +0100

    test(ChallengeStep3): Adds a first level describe to tests

--- src/workflows/WorkflowFactory.spec.ts
+++ src/workflows/WorkflowFactory.spec.ts
@@ -45,42 +45,44 @@ jest.mock('fs', () => ({
   readFileSync: jest.fn(() => workflowYmlContent),
 }));
 
-describe('When creates a workflow with dependant tasks', () => {
-  const clientId = 'client-id';
-  const geoJson = 'geoJsonData';
-  let saveWorkflowSpy: jest.SpyInstance;
-  let saveTaskSpy: jest.SpyInstance;
-  const workflowRepository = AppDataSource.getRepository(Workflow);
-  const taskRepository = AppDataSource.getRepository(Task);
+describe('WorkflowFactory', () => {
+  describe('When creates a workflow with dependant tasks', () => {
+    const clientId = 'client-id';
+    const geoJson = 'geoJsonData';
+    let saveWorkflowSpy: jest.SpyInstance;
+    let saveTaskSpy: jest.SpyInstance;
+    const workflowRepository = AppDataSource.getRepository(Workflow);
+    const taskRepository = AppDataSource.getRepository(Task);
 
-  beforeEach(() => {
-    saveWorkflowSpy = jest.spyOn(workflowRepository, 'save');
-    saveTaskSpy = jest.spyOn(taskRepository, 'save');
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks;
-  });
-
-  it('Should add a dependency field to the task with dependant', async () => {
-    const workflow = new Workflow();
-    workflow.clientId = clientId;
-    saveWorkflowSpy.mockResolvedValue(workflow);
-
-    const taskStep1 = getTask({ step: 1, workflow, type: 'area', geoJson, clientId });
-    const taskStep2 = getTask({
-      step: 2,
-      workflow,
-      type: 'analysis',
-      geoJson,
-      clientId,
-      dependency: taskStep1,
+    beforeEach(() => {
+      saveWorkflowSpy = jest.spyOn(workflowRepository, 'save');
+      saveTaskSpy = jest.spyOn(taskRepository, 'save');
     });
-    saveTaskSpy.mockResolvedValue([taskStep1, taskStep2]);
 
-    const workflowFactory = new WorkflowFactory(AppDataSource);
-    await workflowFactory.createWorkflowFromYAML('path/to/workflow.yml', clientId, geoJson);
+    afterEach(() => {
+      jest.restoreAllMocks;
+    });
 
-    expect(saveTaskSpy).toHaveBeenCalledWith([taskStep1, taskStep2]);
+    it('Should add a dependency field to the task with dependant', async () => {
+      const workflow = new Workflow();
+      workflow.clientId = clientId;
+      saveWorkflowSpy.mockResolvedValue(workflow);
+
+      const taskStep1 = getTask({ step: 1, workflow, type: 'area', geoJson, clientId });
+      const taskStep2 = getTask({
+        step: 2,
+        workflow,
+        type: 'analysis',
+        geoJson,
+        clientId,
+        dependency: taskStep1,
+      });
+      saveTaskSpy.mockResolvedValue([taskStep1, taskStep2]);
+
+      const workflowFactory = new WorkflowFactory(AppDataSource);
+      await workflowFactory.createWorkflowFromYAML('path/to/workflow.yml', clientId, geoJson);
+
+      expect(saveTaskSpy).toHaveBeenCalledWith([taskStep1, taskStep2]);
+    });
   });
 });
